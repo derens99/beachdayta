2f01a5bce16c8e1743eaa286dffa3769f10e9cb0
derens99 <derens99@gmail.com>
2021-04-26 15:19:33 -0400

    testing gui

--- public/map.js
+++ public/map.js
@@ -20,22 +20,29 @@ function newLocation(){
         }
     });
 }
-// var location = currentLocation();
 
 function loadMap(){
     var location = {lat: 42.361145, lng: -71.057083};
     infowindow = new google.maps.InfoWindow();
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 13,
-        center: location
-    });
+    
     newLocation().then(function(response){
         console.log(response.coords.latitude);
         location = {lat: response.coords.latitude, lng: response.coords.longitude};
-        let marker = new google.maps.Marker({
-            position: location,
-            map: map
+        
+        /* TESTING VARIABLES */
+        location = {lat: 41.573360, lng: -70.460670};
+
+        map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 13,
+            center: location
         });
+        
+        // let marker = new google.maps.Marker({
+        //     position: location,
+        //     map: map
+        // });
+
+        createCurrentLocationMarker(location);
 
         const request = {
             radius: 80467,
@@ -56,29 +63,84 @@ function loadMap(){
     })
 }
 
+function createCurrentLocationMarker(location){
+    // var location = {lat: 42.361145, lng: -71.057083};
+    let marker = new google.maps.Marker({
+        position: location
+    });
+    marker.setMap(map);
+    const infowindow = new google.maps.InfoWindow({
+        content: "Current Location",
+        title: "Current Location",
+        maxWidth: 200
+    });
+    marker.addListener("click", () => {
+        infowindow.open(map, marker);
+    });
+}
+
 function createMarker(place) {
     if (!place.geometry || !place.geometry.location) return;
-    // console.log(place);
-
+    console.log(place);
     const marker = new google.maps.Marker({
       map,
       position: place.geometry.location,
       title: place.name,
-     
+        icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
     });
 
-    const placeContent = place.name + "\r" + "Rating: " + place.rating;
+    const request = {
+        placeId: place.place_id
+    };
+
+    service.getDetails(request, (placeObject, status) => {
+        let placeContent;
+        if(status === google.maps.places.PlacesServiceStatus.OK &&
+            placeObject){
+                console.log(placeObject);
+                placeContent = 
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">' + placeObject.name + '</h1>' +
+    '<div id="bodyContent">' +
+    
+    "<p><a href='" + placeObject.url +"'>" + placeObject.formatted_address +"</a>" + "</p>"+
+    "<p>" + "<b>Address: </b>" + placeObject.adr_address + "</p>" +
+    "<p>Rating (out of 5): " + placeObject.rating + "</p>"+
+    "<p>Total Ratings: " + placeObject.user_ratings_total + "</p>"+
+    "</div>" +
+    "</div>";
+            }else{
+            placeContent = 
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">' + place.name + '</h1>' +
+    '<div id="bodyContent">' +
+    "<p>" + place.name +"</p>" +
+    "<p>Rating (out of 5): " + place.rating +"</p>" +
+    "<p>Total Ratings: " + place.user_ratings_total +"</p>" +
+    
+    "</div>" +
+    "</div>";
+            }
+        
+
     const infowindow = new google.maps.InfoWindow({
         content: placeContent,
         title: place.name,
-        maxWidth: 200
+        // maxWidth: 200
     });
     marker.addListener("click", () => {
         infowindow.open(map, marker);
         console.log(place.name);
     });
+    });
+
+    
   }
 
   function createBeachElement(){
-      
+
   }
